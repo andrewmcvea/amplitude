b6f0a61d5e280faafe852b2fae56a6e308bd5379
andrewmcvea <andrewmcvea@uchicago.edu>
2016-07-13 17:03:22 -0500

    Changed from ADC to voltage
    Filtering and graphing is now in volts, although I may want to change it to millivolts since they are pretty small. Also added a small bit to calculate and print the coincidence rate.

--- amplitude.py
+++ amplitude.py
@@ -10,9 +10,11 @@ print "start program"
 
 #Finds the amplitude of each of the charge curves with barrier
 def find_amp(v):
-	amplitude = np.min(v,axis=1)
-	filteramp = amplitude[amplitude < -200]
-	return abs(filteramp)
+        amplitude = np.min(v,axis=1)
+        vamp = adc_to_voltage(amplitude)
+        mask = vamp < -10e-3
+        filteramp = vamp[mask]
+        return abs(filteramp)
 
 #Takes the input from the terminal and reads it
 if __name__ == '__main__':
@@ -30,6 +32,11 @@ if __name__ == '__main__':
     #Signal PMT
     dset2 = f['c2'][:100000]
     histamp2 = find_amp(dset2)
+    
+    l1 = len(histamp)
+    l2 = len(histamp2)
+    c_rate = l2/l1
+    print c_rate
 
     plt.figure(1)
     plt.hist(histamp, bins=range(0, max(histamp), 10))
